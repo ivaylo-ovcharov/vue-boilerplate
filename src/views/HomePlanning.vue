8206afd64987142c0c0810b46e0b652616ca8eab
“IvayloEntropy” <entropyactive@gmail.com>
2023-04-30 12:41:06 +0300

    add routing

---
+++ src/views/HomePlanning.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { peopleHeader } from './people.config'
+
+const { fetchPeople } = usePeopleLoader
+
+const selecetedPersonId = ref('')
+const searchTerm = ref('')
+const currentPage = ref('')
+
+const people = fetchPeople(currentPage, searchTerm)
+</script>
+
+<template>
+  <SearchTerm v-model="searchTerm" />
+  <DataTable :headers="peopleHeader" :data="people" />
+  <Pagination v-model="currentPage" />
+  <PlanetModal :selectedPersonId="selecetedPersonId" />
+</template>
---
+++ src/views/Modal.vue
@@ -0,0 +1,16 @@
+<script setup>
+const { fetchPlanet } = usePlanetLoader
+
+const props = defineProps({
+  selectedPersonId: { type: Number, required: true },
+})
+
+const planet = fetchPlanet(selectedPersonId)
+</script>
+<template>
+  <SwModal title="Selected user name's modal">
+    <p>Planet name: {{ planet.name }}</p>
+    <p>Planet mass: {{ planet.mass }}</p>
+    <SwButton variant="primary" @click="emit('close')">Got it</SwButton>
+  </SwModal>
+</template>
